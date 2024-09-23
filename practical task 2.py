ce2112f41793bbe5dba4051ee85013a265208490
Indigo-z <merccarmela@gmail.com>
2024-09-23 16:23:04 +0200

    Add files via upload

---
+++ practical task 2.py
@@ -0,0 +1,4 @@
+number_of_homework = "12"
+number_of_hours = "1.5"
+name = "Python"
+print('Курс:',name,',','всего задач:',number_of_homework,',' ' затрачено часов:',number_of_hours,','' среднее время выполнения', 1.5 / 12 )
