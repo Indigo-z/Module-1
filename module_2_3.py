faa7a74629ca03db5aba6d5464bcc2edd81c53f7
Indigo-z <merccarmela@gmail.com>
2024-09-29 20:52:34 +0200

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+positive_numbers = []
+while True:
+    for number in my_list:
+        if number > 0:
+            positive_numbers.append(number)
+    print(positive_numbers)
+    break
+
+
+
+
+
