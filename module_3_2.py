8703fefddf42958e0c0cf66dc28ba564d484f66f
Indigo-z <merccarmela@gmail.com>
2024-10-11 16:10:31 +0200

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,22 +1,23 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if "@" not in recipient or not (
+            recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
 
+    if "@" not in sender or not (sender.endswith(".com") or sender.endswith(".ru") or sender.endswith(".net")):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
 
-    valid_recipient = "@" in recipient or (recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net"))
-    valid_sender = sender.endswith(".com") or sender.endswith(".ru") or sender.endswith(".net")
 
     if recipient == sender:
-            print("Нельзя отправить письмо самому себе!")
-            return
+        print("Нельзя отправить письмо самому себе!")
+        return
 
 
-    if not valid_recipient or not valid_sender:
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    if sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
     else:
-        if sender == "university.help@gmail.com":
-            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-        else:
-            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
