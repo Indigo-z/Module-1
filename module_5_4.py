69cefb2510070800d8ae4315402316edfe3ec3db
Indigo-z <merccarmela@gmail.com>
2024-10-28 12:04:36 +0100

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,38 @@
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        instance = super().__new__(cls)
+        cls.houses_history.append(args[0])
+        return instance
+
+    def __del__(self):
+        print(f'{self.name} снесен, но останется в истории')
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
