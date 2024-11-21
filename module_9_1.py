a55b2eb4a0a86c1aeb7b9ae5ecb24c7037328261
Indigo-z <merccarmela@gmail.com>
2024-11-21 10:38:40 +0100

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,13 @@
+
+def apply_all_func(int_list, *functions):
+    result = {}
+    for func in functions:
+        try:
+            result[func.__name__] = func(int_list)
+        except TypeError:
+            print(f'[{func.__name__} не является функцией')
+    return result
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
