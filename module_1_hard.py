f24f444632dd05d2777329ed7e46f9b216945693
Indigo-z <merccarmela@gmail.com>
2024-12-08 20:01:22 +0100

    Add files via upload

---
+++ module_1_hard.py
@@ -0,0 +1,13 @@
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+st = sorted(students)
+gr = []
+
+for num in grades:
+    i = sum(num) / len(num)
+    gr.append(i)
+
+my_dict = dict(zip(st,gr))
+print(my_dict)
+
